4fa412c2d4e8c6b25704e16775796b501ecfc7e8
Saiteja4041 <saitejadevineni@gmail.com>
2022-06-28 14:29:34 +0000

    react router test project

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="home"
+      className="logo"
+    />
+    <h1 className="heading">About</h1>
+    <p className="info">
+      I'm a Front-End Developer, I Love to develop scalable, performant and
+      beautiful websites.
+    </p>
+  </div>
+)
+
+export default About
---
+++ src/components/Header/index.js
@@ -0,0 +1,37 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+          alt="wave"
+          className="wave-logo"
+        />
+        <h1 className="text"> Wave</h1>
+      </div>
+      <ul className="nav-list">
+        <li>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="nav-link">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Header
